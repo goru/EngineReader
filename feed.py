c08048fb41986941dd0d62362e43d3330547749e
lostman-github <lostman.worlds.end+github@gmail.com>
2013-06-29 03:00:43 +0900

    first commit

---
+++ feed.py
@@ -0,0 +1,52 @@
+from xml.dom import minidom
+
+class FeedParserBuilder():
+    @classmethod
+    def build(cls, text):
+        dom = minidom.parseString(text)
+
+        parsers = [ AtomParser ]
+        for parser in parsers:
+            f = parser.parse(dom)
+            if (f != None):
+                return f
+
+        return None
+
+class FeedParser():
+    doc = None
+
+    @classmethod
+    def parse(cls, dom):
+        return None
+
+    def getNodeData(self, elm):
+        for node in elm.childNodes:
+            if node.nodeType == node.TEXT_NODE:
+                return node.data
+
+        return None
+
+class AtomParser(FeedParser):
+    @classmethod
+    def parse(cls, dom):
+        doc = dom.documentElement
+        if doc.tagName == 'feed' and doc.getAttribute('xmlns') == 'http://www.w3.org/2005/Atom':
+            feed = AtomParser()
+            feed.doc = doc
+            return feed
+
+        return None
+
+    def entries(self):
+        entries = []
+
+        for child in self.doc.childNodes:
+            if child.nodeType != child.ELEMENT_NODE or child.nodeName != 'entry':
+                continue
+            
+            entry = {'title': self.getNodeData(child.getElementsByTagName('title')[0]),
+                      'link': child.getElementsByTagName('link')[0].getAttribute('href')}
+            entries.append(entry)
+
+        return entries
---
+++ reader.py
@@ -0,0 +1,100 @@
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+import encodings
+import json
+import time
+import urllib2
+
+def decode(s):
+    for e in encodings.aliases.aliases.values():
+        try:
+            return s.decode(e)
+        except:
+            pass
+    return s
+
+class FeedModel(db.Model):
+    name = db.StringProperty(multiline=False)
+    url = db.StringProperty(multiline=False)
+    created = db.DateTimeProperty(auto_now_add = True)
+    modified = db.DateTimeProperty(auto_now = True)
+    body = db.TextProperty()
+
+    def fromDict(self, dic):
+        self.name = dic['name']
+        self.url = dic['url']
+        #self.body = decode(urllib2.urlopen(self.url).read())
+
+    def toDict(self):
+        return {'name': self.name,
+                 'url': self.url,
+             'created': int(time.mktime(self.created.timetuple())),
+            'modified': int(time.mktime(self.modified.timetuple())),
+                #'body': self.body,
+                  'id': self.key().id()}
+
+class FeedItemModel(db.Model):
+    feed = db.ReferenceProperty(FeedModel)
+
+class HandlerBase(webapp.RequestHandler):
+    def dumpsJSON(self, obj):
+        return json.dumps(obj, indent=2, sort_keys=True)
+
+    def loadsJSON(self, text):
+        return json.loads(text)
+
+class FeedCollectionHandler(HandlerBase):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        feeds = []
+        for feed in FeedModel.all():
+            feeds.append(feed.toDict())
+
+        self.response.out.write(self.dumpsJSON({'feeds': feeds}))
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        feed = FeedModel()
+        feed.fromDict(self.loadsJSON(self.request.body))
+        feed.put()
+
+        self.response.out.write(self.dumpsJSON(feed.toDict()))
+
+class FeedHandler(HandlerBase):
+    def getFeed(self, feedId):
+        feed = FeedModel.get_by_id(long(feedId))
+        if feed == None:
+            self.error(404)
+            self.response.out.write(self.dumpsJSON({}))
+        return feed
+
+    def get(self, feedId):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        feed = self.getFeed(feedId)
+        if feed != None:
+            self.response.out.write(self.dumpsJSON(feed.toDict()))
+
+    def post(self, feedId):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        feed = self.getFeed(feedId)
+        if feed != None:
+            feed.fromDict(self.loadsJSON(self.request.body))
+            feed.put()
+            self.response.out.write(self.dumpsJSON(feed.toDict()))
+
+application = webapp.WSGIApplication(
+    [('/feed/?', FeedCollectionHandler),
+    ('/feed/(\d+)/?', FeedHandler)],
+    debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
